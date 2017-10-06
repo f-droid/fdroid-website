dc55d1c48a9430609497810be535e6166cc7e274
Hans-Christoph Steiner <hans@eds.org>
2017-10-06 12:30:47 +0200

    check translated strings for proper use of formats
    Formats are the most error prone part of the translation process.

---
+++ tools/check-format-strings.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import glob
+import json
+import os
+import re
+import sys
+
+
+def make_flat_dict(d):
+    flat_dict = dict()
+
+    def find(sectionlist, d):
+        for k, v in d.items():
+            currentsection = sectionlist + [k]
+            if isinstance(v, dict):
+                find(currentsection, v)
+            else:
+                flat_dict['.'.join(currentsection)] = v
+    find([], d)
+    return(flat_dict)
+
+
+basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+datadir = os.path.join(basedir, '_data')
+
+formatpattern = re.compile(r'(\[[a-z][a-z0-9_]*\])([^(]|$)')
+bracketspattern = re.compile(r'[\[\]][^(]')
+
+with open(os.path.join(datadir, 'strings.json')) as fp:
+    sourcestrings = make_flat_dict(json.load(fp))
+
+formats = dict()
+for k, v in sourcestrings.items():
+    m = formatpattern.search(v)
+    if m:
+        formats[k] = m.group(1)
+        print(k, '\tformat:', m.group(1))
+
+exitvalue = 0
+for f in glob.glob(os.path.join(datadir, '*', 'strings.json')):
+    #print(f)
+    with open(f) as fp:
+        targetstrings = make_flat_dict(json.load(fp))
+    for k, v in targetstrings.items():
+        if k in formats and formats[k] not in v:
+            print(os.path.relpath(f) + ':\n\t', k, 'should have', formats[k] + ':\n\t', v)
+            exitvalue += 1
+
+sys.exit(exitvalue)
