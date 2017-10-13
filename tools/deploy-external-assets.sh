054a216424704426e21fc01eba7ae5036f0dd7eb
Hans-Christoph Steiner <hans@eds.org>
2017-10-13 11:39:05 +0200

    fix deploying badge/ and old forums/ archive

--- tools/deploy-external-assets.sh
+++ tools/deploy-external-assets.sh
@@ -11,17 +11,19 @@ fi
 OUTPUT_DIR=$1
 
 echo "Deploying the /badge/ directory..."
-BADGE_DIR=${OUTPUT_DIR}/badge
-curl https://gitlab.com/fdroid/artwork/repository/archive.tar.gz?ref=master | gunzip -c | tar --wildcards -xf - 'artwork-master*/badge'
-rm -rf ${BADGE_DIR}
-mv artwork-master-*/badge ${BADGE_DIR}
+curl https://gitlab.com/fdroid/artwork/repository/archive.tar.gz?ref=master \
+    | gunzip -c \
+    | tar --wildcards -xf - 'artwork-master*/badge'
+rm -rf ${OUTPUT_DIR}/badge
+mv artwork-master-*/badge ${OUTPUT_DIR}/
 rm -r artwork-master-*
 
 echo "Deploying the /forums/ directory..."
-FORUMS_DIR=${OUTPUT_DIR}/forums
-curl https://gitlab.com/fdroid/fdroid-website-legacy-forum/repository/archive.tar.gz?ref=master | gunzip -c | tar --wildcards -xf - 'fdroid-website-legacy-forum-master*/forums'
-rm -rf ${FORUMS_DIR}
-mv fdroid-website-legacy-forum-master-*/forums ${FORUMS_DIR}
+curl https://gitlab.com/fdroid/fdroid-website-legacy-forum/repository/archive.tar.gz?ref=master \
+    | gunzip -c \
+    | tar --wildcards -xf - 'fdroid-website-legacy-forum-master*/forums'
+rm -rf ${OUTPUT_DIR}/forums
+mv fdroid-website-legacy-forum-master-*/forums ${OUTPUT_DIR}/
 rm -r fdroid-website-legacy-forum-master*
 
 echo "Deploying download button symlinks..."
