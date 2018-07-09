6df308cfe249435b804f5cb21c34dd34e2221f19
Hans-Christoph Steiner <hans@eds.org>
2018-07-09 11:38:40 +0200

    make ./tools/check-page-links.py provide clearer error output

--- tools/check-page-links.py
+++ tools/check-page-links.py
@@ -13,27 +13,27 @@ errorcount = 0
 pattern = re.compile(r'](\([^h][^\)]+\))')
 bad_md_link = re.compile(r'.*\]\s+\(')
 for f in sorted(glob.glob('po/*.po*')):
-    print('\n', f, '==================================================================')
+    output = ''
     with open(f, 'r') as fp:
         catalog = read_po(fp)
     for message in catalog:
         if 'type: Title #' in message.auto_comments:
             if ':' in message.string and message.string[0] not in ('"', "'"):
                 errorcount += 1
-                print('colons not allowed in titles without quotes: %s "%s" "%s"'
+                output += ('colons not allowed in titles without quotes: %s "%s" "%s"'
                       % (f, message.id, message.string))
             if message.string:
                 if '"' in (message.string[0], message.string[-1]) and message.string.count('"') % 2:
                     errorcount += 1
-                    print('Mismatched " in title:', message.string)
+                    output += 'Mismatched " in title:' + message.string
                 if "'" in (message.string[0], message.string[-1]) and message.string.count("'") % 2:
                     errorcount += 1
-                    print("Mismatched ' in title:", message.string)
+                    output += "Mismatched ' in title:" + message.string
 
         m = bad_md_link.search(message.string)
         if m:
             errorcount += 1
-            print('Space breaks Markdown link:', message.id, '\n:', m.group())
+            output += 'Space breaks Markdown link: ' + message.id, '\n: ' + m.group()
 
         idlinks = []
         for m in pattern.findall(message.id):
@@ -42,19 +42,21 @@ for f in sorted(glob.glob('po/*.po*')):
         for m in pattern.findall(message.string):
             strlinks.append(m)
         if len(strlinks) > 0 and len(idlinks) != len(strlinks):
-            print('ERROR', f, len(idlinks), ' != ', len(strlinks), message.id)
+            output += 'ERROR ' + f + ' ' + str(len(idlinks)) + ' != ' + str(len(strlinks)) + ' ' + message.id
             errorcount += 1
         for i in range(len(strlinks)):
             if message.string and idlinks[i] != strlinks[i]:
                 errorcount += 1
-                print(f, 'msgstr', idlinks[i], '!=', strlinks[i])
+                output += f + '\nmsgstr    ' + idlinks[i] + '\n !=       ' + strlinks[i]
                 # inputf = f + '.orig'
                 # shutil.copy(f, inputf)
                 # cmd = ('msgfilter --input=' + inputf + ' --output-file=' + f
                 #        + ''' python3 -c 'import sys; sys.stdout.write(sys.stdin.read().replace("""'''
                 #        + strlinks[i] + '""", """' + idlinks[i] + '"""))\'')
-                # print(cmd)
+                # output += (cmd)
                 # os.system(cmd)
                 # os.remove(inputf)
+    if output:
+        print('\n', f, '==================================================================\n', output)
 
 sys.exit(errorcount)
