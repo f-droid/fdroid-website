1dd5c755e80438f8fd9a9ea69c7949aba27d8a0e
Hans-Christoph Steiner <hans@eds.org>
2019-04-24 10:56:01 +0200

    only include languages with strings.json at least 50% translated
    @kingu enabled the Weblate "Language consistency" plugin, which created
    blank translations in all Weblate components for every language supported
    anywhere in F-Droid.

--- tools/build-all-languages.sh
+++ tools/build-all-languages.sh
@@ -6,10 +6,14 @@ git checkout master
 git fetch https://hosted.weblate.org/git/f-droid/website
 git reset --hard FETCH_HEAD
 
+minsize=`ls -l _data/strings.json | awk '{ print ($5 * 0.50) }'`
+
 # 'en' needs to be manually included in the list since there isn't a
 # translation file for it.
 languages="en,"
 for f in _data/[a-z][a-z]*/strings.json; do
+    size=`ls -l $f | awk '{ print $5 }'`
+    test $size -gt $minsize || continue
     locale=`echo $f | sed 's,.*/\([a-z][a-z][a-zA-Z0-9_-]*\)/strings.json,\1,'`
     if [ -e po/_pages.${locale}.po ]; then
         languages="$languages $locale,"
