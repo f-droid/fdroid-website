e1adad5ca77de91a95da5d8008f1c2ed3294af99
Hans-Christoph Steiner <hans@eds.org>
2020-03-31 17:59:17 +0200

    tools/prepare-for-deploy.sh: convert to bash script to avoid extra deps
    Since this script runs as the first step in the Docker image, it needs to
    have as few dependencies as possible.
    
    # Conflicts:
    #	tools/prepare-for-deploy.py

---
+++ tools/prepare-for-deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# This script sets up this git repo for the final deploy process to
+# https://f-droid.org.  It will automatically delete changes in this
+# local git repo that are not present in the git tag that is to be
+# deployed.  It is meant to be automatically triggered whenever the
+# new index.jar has been published on f-droid.org.
+#
+# This script assumes two things:
+# 1. the deploy machine includes the public GPG keys for tag verification
+# 2. the 'origin' remote is set to https://gitlab.com/fdroid/fdroid-website.git
+
+set -e
+set -x
+
+apt-get update
+apt-get -qy install --no-install-recommends ca-certificates git
+
+git remote update --prune
+git fetch --tags
+
+for tag in `git tag --sort=-taggerdate`; do
+    if git tag -v $tag; then
+	git clean -fdx
+	git checkout -B master $tag
+        echo "Set up $tag to deploy!"
+	exit
+    fi
+done
+
+echo 'ERROR: could not find any signed release tags!'
+exit(1)
