fc5dd79ca8f6db71bb3b35c4894cdec60fcbce3b
Hans-Christoph Steiner <hans@eds.org>
2020-05-05 17:07:58 +0200

    script to report which languages are ready to launch on f-droid.org
    Must be 99% translated on:
    * F-Droid client
    * website "chrome" strings
    * website-pages

--- tools/check-language-status.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# The CSV from Weblate contains the following header row:
-# name,code,total,translated,translated_percent,total_words,translated_words,failing,failing_percent,fuzzy,fuzzy_percent,url_translate,url,last_change,last_author
-# where the columns of interest are $5 (translated_percent) and $2 (code).
-
-for f in f-droid fdroiddata repomaker repomaker-javascript website website-docs website-posts; do
-    echo $f `curl --silent https://hosted.weblate.org/exports/stats/f-droid/$f/\?format\=csv | tail -n +2 | awk -F ',' '{ if ($5 > 95.0) print $2 }' | sort -u`
-done
