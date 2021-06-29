918562228aa03adaa1c80a98431618818df7950b
Hans-Christoph Steiner <hans@eds.org>
2021-06-29 09:28:31 +0200

    tools/check-asset-tags.sh: ignore _site/ and reformat code
    _site is the generated HTML, so the pattern was matching lots of things,
    but wrongly.  Also, set -e was useless since both greps had || true.

--- tools/check-asset-tags.sh
+++ tools/check-asset-tags.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
-set -e
-grep -ER --exclude-dir=.git --exclude='*~' --exclude='*.swp' \
-  --exclude-dir=tools --exclude-dir=_plugins \
-  --color=auto '\{%\s*assets' . || true
-grep -ER --exclude-dir=.git --exclude='*~' --exclude='*.swp' \
-  --exclude-dir=tools --exclude-dir=_plugins \
-  --exclude='*.webmanifest' --exclude='*.xml' --exclude='*.scss' \
+
+EXITVALUE=0
+
+grep -ER \
+  --exclude-dir=.git \
+  --exclude='*~' \
+  --exclude='*.swp' \
+  --exclude-dir=tools \
+  --exclude-dir=_plugins \
+  --exclude-dir=_site \
+  --color=auto '\{%\s*assets' . \
+    || EXITVALUE=1
+
+grep -ER \
+  --exclude-dir=.git \
+  --exclude='*~' \
+  --exclude='*.swp' \
+  --exclude-dir=tools \
+  --exclude-dir=_plugins \
+  --exclude-dir=_site \
+  --exclude='*.webmanifest' \
+  --exclude='*.xml' \
+  --exclude='*.scss' \
   --exclude=Second_Audit_Report.html \
-  --color=auto '[^a-z]/assets/' . || true
+  --color=auto '[^a-z]/assets/' . \
+    || EXITVALUE=1
+
+exit $EXITVALUE
