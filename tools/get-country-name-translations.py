969a30b994e264a001b45fc2d4d8e6892bfc6968
Hans-Christoph Steiner <hans@eds.org>
2021-09-20 19:21:37 +0200

    Running a Mirror: mv rsync commands to data, no need to translate

---
+++ tools/get-country-name-translations.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import glob
+import json
+import os
+import requests
+import yaml
+
+os.chdir(os.path.join(os.path.dirname(__file__), '..'))
+
+baseurl = 'https://raw.githubusercontent.com/umpirsky/country-list/master/data'
+
+with open('_data/rsync_mirrors.yaml') as fp:
+    codes = sorted(yaml.safe_load(fp).keys())
+
+for f in sorted(glob.glob('_data/*/strings.json')):
+    target = os.path.basename(os.path.dirname(f))
+    url = os.path.join(baseurl, target, 'country.json')
+    r = requests.get(url)
+    if r.status_code == 200:
+        data = r.json()
+        with open(f) as fp:
+            strings = json.load(fp)
+        for code in codes:
+            translation = data.get(code.upper())
+            if translation:
+                print(url, code, translation)
+                if 'mirrors' not in strings:
+                    strings['mirrors'] = {}
+                strings['mirrors'][code] = translation
+                with open(f, 'w') as fp:
+                    json.dump(strings, fp, ensure_ascii=False, indent=4, sort_keys=True)
+                    fp.write('\n')  # Weblate seems to do this
