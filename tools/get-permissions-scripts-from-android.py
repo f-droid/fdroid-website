b777bbd833b45edae366731a98d9c35f2daf779f
Hans-Christoph Steiner <hans@eds.org>
2023-10-16 16:57:49 +0200

    tools/get-permissions-scripts-from-android.py: remove unused string
    permission_request_notification_for_app_with_subtitle is not used by the
    website at all, it must have slipped in somehow.

--- tools/get-permissions-scripts-from-android.py
+++ tools/get-permissions-scripts-from-android.py
@@ -12,6 +12,7 @@ skipkeys = (
     'perm_costs_money',
     'permission_request_notification_title',
     'permission_request_notification_with_subtitle',
+    'permission_request_notification_for_app_with_subtitle',
     'perms_description_app',
 )
 
@@ -39,7 +40,6 @@ overwritekeys = (
     "permgroupdesc_nearby_devices",
     "permgrouplab_nearby_devices",
     "permgrouplab_storage",
-    "permission_request_notification_for_app_with_subtitle",
     "permlab_backgroundCamera",
     "permlab_bluetooth_advertise",
     "permlab_bluetooth_connect",
