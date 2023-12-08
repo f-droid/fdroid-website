527424f9472a87f6542ba8b6c8bea79220f9b2ef
Michael Pöhn <michael@poehn.at>
2023-12-08 11:09:06 +0100

    ↩️  make-release-tag.sh: add newlines

--- tools/make-release-tag.sh
+++ tools/make-release-tag.sh
@@ -1,4 +1,11 @@
 #!/bin/sh -ex
 
-NEXT_TAG=$(git tag | grep -E '^[0-9]\.[0-9]+$' | python3 -c 'import sys; from packaging import version as v; l=sorted([v.parse(x) for x in sys.stdin.readlines()])[-1]; print("{}.{}".format(l.major, l.minor+1))')
+NEXT_TAG=$(git tag | grep -E '^[0-9]\.[0-9]+$' | python3 -c '\
+import sys; \
+from packaging import version; \
+\
+l=sorted([version.parse(x) for x in sys.stdin.readlines()])[-1]; \
+print("{}.{}".format(l.major, l.minor+1)) \
+')
+
 git tag --sign $NEXT_TAG --message "tagging release v$NEXT_TAG" $1
