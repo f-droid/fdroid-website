271aa1b137b27e3c9c26d78df3ca5409992a45c7
g0t mi1k <have.you.g0tmi1k@gmail.com>
2024-07-06 15:49:29 +0100

    i18n: Tabs to spaces

--- tools/i18n.sh
+++ tools/i18n.sh
@@ -42,7 +42,7 @@ EOF
         # for now, new TWIF posts are not translated
         [[ $f == _posts/202*twif*.md ]] && continue
         [[ $f == _posts/202*this-week-in-*.md ]] && continue
-	echo "[type: markdown] $f \$lang:$section/\$lang/$(basename $f)" >> $po4a_conf
+        echo "[type: markdown] $f \$lang:$section/\$lang/$(basename $f)" >> $po4a_conf
     done
     po4a --verbose $po4a_conf &
     eval pid${section}=$!
